0e4c2d4d8d2251d490a0e3bcd47a95a4bddec599
Metin Keleş <metinkeles677@gmail.com>
2023-06-30 14:20:43 +0300

    tailwindVariables&FontSettings

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-
+      colors: {
+        primary: "#ffbe33",
+        secondary: "#222831"
+      },
+      fontFamily: {
+        dancing: ["Dancing Script", "cursive"],
+      }
     },
   },
   plugins: [],
