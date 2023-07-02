1bea050a4676529b6201d8c6cecd4c1f2f3ceb67
Metin Keleş <metinkeles677@gmail.com>
2023-07-02 10:33:14 +0300

    carouselComponent

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Carousel from '../../components/Carousel'
 
 function Index() {
   return (
-    <div></div>
+    <div className='relative'>
+      <Carousel/>
+    </div>
   )
 }
 
