d095170242325b1d5bb4df2ba87d302100df3b14
Metin Keleş <metinkeles677@gmail.com>
2023-07-03 13:19:13 +0300

    reservationInputDesign

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -2,17 +2,48 @@ import Input from './form/Input'
 import Title from './ui/Title'
 
 const Reservation = () => {
+    const inputs = [
+        {
+            id:1,
+            name:"fullName",
+            type: "text",
+            placeholder: "Your Full Name"
+        },
+        {
+            id: 2,
+            name: "phoneNumber",
+            type: "number",
+            placeholder: "Your Phone Number"
+        },
+        {
+            id: 3,
+            name: "email",
+            type: "email",
+            placeholder: "Your Email Address"
+        },
+        {
+            id: 4,
+            name: "persons",
+            type: "number",
+            placeholder: "How Many Persons?"
+        },
+        {
+            id: 5,
+            name: "date",
+            type: "datetime-local",
+            placeholder: "Date "
+        }
+    ]
+
   return (
     <div className='container mx-auto py-12'>
         <Title addClass="text-[40px] mb-10">Book A Table</Title>
         <div className="flex justify-between flex-wrap-reverse gap-x-10">
             <div className='lg:flex-1 w-full'>
                 <div className='flex flex-col gap-y-3'>
-                    <Input/>
-                    <Input/>
-                    <Input/>
-                    <Input/>
-                    <Input/>
+                    {inputs.map((input) => (
+                        <Input key={input.id} {...input} />
+                    ))}
                 </div>
                 <button className="btn-primary my-4">Book Now</button>
             </div>
