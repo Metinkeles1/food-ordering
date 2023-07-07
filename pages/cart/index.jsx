465369f7183677653170285a2ea949b030160a87
Metin Keleş <metinkeles677@gmail.com>
2023-07-07 10:57:44 +0300

    cartPageTable

--- pages/cart/index.jsx
+++ pages/cart/index.jsx
@@ -4,33 +4,45 @@ import Title from "../../components/ui/Title";
 const Cart = () => {
   return (
     <div className='min-h-[calc(100vh_-_433px)]'>
-      <div className='flex justify-between'>
-        <div className='min-h-[calc(100vh_-_433px)] flex items-center flex-1'>
-          <table>
-            <thead>
+      <div className='flex justify-between items-center md:flex-row flex-col'>
+        <div className='min-h-[calc(100vh_-_433px)] flex items-center flex-1 p-10 overflow-x-auto w-full'>
+          <table className='w-full text-sm text-center text-gray-secondary min-w-[1000px]'>
+            <thead className='text-xs text-gray-200 uppercase bg-gray-700'>
               <tr>
-                <th>PRODUCT</th>
-                <th>EXTRAS</th>
-                <th>PRICE</th>
-                <th>QUANTITY</th>
+                <th scope='col' className='py-3 px-6'>
+                  PRODUCT
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  EXTRAS
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  PRICE
+                </th>
+                <th scope='col' className='py-3 px-6'>
+                  QUANTITY
+                </th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td>
+              <tr className='border-b bg-[#fff] border-gray-700 hover:bg-primary hover:text-[#fff] transition-all'>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary flex items-center gap-x-2 justify-center'>
                   <Image src='/images/f1.png' alt='' width='50' height='50' />
                   <span>Good Pizza</span>
                 </td>
-                <td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
                   <span>mayonez, acı sos, ketçap</span>
                 </td>
-                <td>$20</td>
-                <td>1</td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
+                  $20
+                </td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
+                  1
+                </td>
               </tr>
             </tbody>
           </table>
         </div>
-        <div className='bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center p-12'>
+        <div className='bg-secondary min-h-[calc(100vh_-_433px)] flex flex-col justify-center p-12 md:w-auto w-full md:text-start text-center'>
           <Title addClass='text-[40px]'>CART TOTAL</Title>
           <div className='mt-6'>
             <b>Subtotal: </b>$20 <br />
@@ -38,7 +50,11 @@ const Cart = () => {
             <b>Total: </b>$20
           </div>
 
-          <button className='btn-primary mt-4'>CHECKOUT NOW</button>
+          <div>
+            <button className='btn-primary mt-4 md:w-auto w-52 text-center'>
+              CHECKOUT NOW
+            </button>
+          </div>
         </div>
       </div>
     </div>
