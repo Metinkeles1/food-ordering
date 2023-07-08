7fb16a9d7b7d3fc175a1afdec0eace1c632592f5
Metin Keleş <metinkeles677@gmail.com>
2023-07-08 10:12:35 +0300

    profileCard

---
+++ pages/profile/index.jsx
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { FaKey, FaHome, FaSignOutAlt } from "react-icons/fa";
+import { RiEBike2Fill } from "react-icons/ri";
+
+const Index = () => {
+  return (
+    <div className='min-h-[calc(100vh_-_433px)] pt-10'>
+      <div className='flex px-10 '>
+        <div className='border w-80'>
+          <div className='relative flex flex-col items-center px-10 py-5 '>
+            <Image
+              src='/images/client2.jpg'
+              alt=''
+              width={100}
+              height={100}
+              className='rounded-full'
+            />
+            <b className='text-2xl mt-1'>John Doe</b>
+          </div>
+          <ul className='text-center font-semibold'>
+            <li className='border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all flex items-center justify-center'>
+              <FaHome />
+              <button className='ml-1'>Account</button>
+            </li>
+            <li className='border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all flex items-center justify-center'>
+              <FaKey />
+              <button className='ml-1'>Password</button>
+            </li>
+            <li className='border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all flex items-center justify-center'>
+              <RiEBike2Fill />
+              <button className='ml-1'>Orders</button>
+            </li>
+            <li className='border w-full p-3 cursor-pointer hover:bg-primary hover:text-white transition-all flex items-center justify-center'>
+              <FaSignOutAlt />
+              <button className='ml-1'>Exit</button>
+            </li>
+          </ul>
+        </div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
