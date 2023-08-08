0cf8dc798ac2c9039e74c41ac01dc45e7ad9a04b
Metin Keleş <metinkeles677@gmail.com>
2023-08-08 22:58:58 +0300

    categoryOperations

--- components/admin/Category.jsx
+++ components/admin/Category.jsx
@@ -1,39 +1,74 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Input from "../form/Input";
 import Title from "../ui/Title";
+import axios from "axios";
 
 const Category = () => {
   const [inputText, setInputText] = useState("");
-  const [categories, setCategories] = useState(["Pizza", "Hamburger"]);
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/categories`
+        );
+        setCategories(res?.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getCategories();
+  }, []);
+
+  const handleCreate = async (e) => {
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/categories`,
+        { title: inputText }
+      );
+      setCategories([...categories, res.data]);
+      setInputText("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+    try {
+      if (confirm("Are you sure you want to delete this category?")) {
+        await axios.delete(
+          `${process.env.NEXT_PUBLIC_API_URL}/categories/${id}`
+        );
+        setCategories(categories.filter((cat) => cat._id !== id));
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className='flex-1 lg:p-8 lg:mt-0 mt-5'>
       <Title addClass='text-[40px] mb-4'>Category</Title>
-      <div className='mt-5'>
+      <div className='mt-5 '>
         <div className='flex flex-1 gap-4 items-center'>
           <Input
             placeholder='Add a new Category...'
             onChange={(e) => setInputText(e.target.value)}
             value={inputText}
           />
-          <button
-            className='btn-primary '
-            onClick={() => {
-              setCategories([...categories, inputText]);
-              setInputText("");
-            }}
-          >
+          <button className='btn-primary ' onClick={handleCreate}>
             Add
           </button>
         </div>
-        <div className='mt-10'>
-          {categories.map((category, index) => (
-            <div className='flex justify-between mt-4' key={index}>
-              <b className='text-xl'>{category}</b>
+        <div className='mt-10 max-h-[250px] overflow-auto pb-4'>
+          {categories.map((category) => (
+            <div className='flex justify-between mt-4' key={category._id}>
+              <b className='text-xl'>{category.title}</b>
               <button
                 className='btn-primary !bg-danger'
-                onClick={() =>
-                  setCategories(categories.filter((cat) => cat !== category))
-                }
+                onClick={(e) => handleDelete(e, category._id)}
               >
                 Delete
               </button>
