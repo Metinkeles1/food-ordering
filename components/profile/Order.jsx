a7c0362d52a7cb48d8ca30a99c89e693488601bb
Metin Keleş <metinkeles677@gmail.com>
2023-12-11 10:17:10 +0300

    operationsProfileOrders

--- components/profile/Order.jsx
+++ components/profile/Order.jsx
@@ -1,7 +1,30 @@
 import Image from "next/image";
 import Title from "../ui/Title";
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import axios from "axios";
 
 const Order = () => {
+  const [data, setData] = useState([]);
+  const status = ["preparing", "On the way", "delivered"];
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const getOrders = async () => {
+      try {
+        const res = await axios.get(
+          `${process.env.NEXT_PUBLIC_API_URL}/orders`
+        );
+        setData(
+          res?.data.filter((order) => order.email === session.user.email)
+        );
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getOrders();
+  }, [session]);
+
   return (
     <div className='flex-1 lg:p-8 lg:mt-0 mt-5'>
       <Title addClass='text-[40px] mb-4'>Orders</Title>
@@ -27,25 +50,30 @@ const Order = () => {
             </tr>
           </thead>
           <tbody>
-            <tr className='border-b bg-[#fff] border-gray-700 hover:bg-primary hover:text-[#fff] transition-all'>
-              <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary flex items-center gap-x-2 justify-center'>
-                <span>63107</span>
-              </td>
-              <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
-                <span>Istanbul</span>
-              </td>
-              <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
-                01-09-2022
-              </td>
-              <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
-                $18
-              </td>
-              <td className='px-6 py-4 whitespace-nowrap'>
-                <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
-                  Preparing
-                </span>
-              </td>
-            </tr>
+            {data.map((order) => (
+              <tr
+                key={order._id}
+                className='border-b bg-[#fff] border-gray-700 hover:bg-primary hover:text-[#fff] transition-all'
+              >
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary flex items-center gap-x-2 justify-center'>
+                  <span>{order._id.substring(0, 6)}...</span>
+                </td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
+                  <span>{order.address}</span>
+                </td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
+                  {order.createdAt}
+                </td>
+                <td className='py-4 px-6 font-medium whitespace-nowrap hover:text-secondary'>
+                  ${order.total}
+                </td>
+                <td className='px-6 py-4 whitespace-nowrap'>
+                  <span className='px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800'>
+                    {status[order?.status]}
+                  </span>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
