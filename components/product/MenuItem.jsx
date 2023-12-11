135fbe265c03e561ac86f6e850cf977282774497
Metin Keleş <metinkeles677@gmail.com>
2023-12-11 08:46:11 +0300

    menuItemCssFixed

--- components/product/MenuItem.jsx
+++ components/product/MenuItem.jsx
@@ -14,7 +14,7 @@ const MenuItem = ({ product }) => {
       </div>
       <div className='p-[25px] text-white'>
         <h4 className='text-xl font-semibold'>{product.title}</h4>
-        <p className='text-[15px]'>{product.desc}</p>
+        <p className='text-[15px]'>{product.desc.substring(0, 150)}...</p>
         <div className='flex justify-between items-center mt-3'>
           <span>${product.prices[0]}</span>
           <button className='btn-primary w-10 h-10 rounded-full !p-0 grid place-content-center'>
