7a5c818f02a2fb413f65f1fa892e9876dc7694f5
Metin Keleş <metinkeles677@gmail.com>
2024-01-12 23:54:38 +0300

    Implement campaign population in product API routes

--- pages/api/products/campaigns/index.js
+++ pages/api/products/campaigns/index.js
@@ -1,17 +1,22 @@
-// api/products/campaigns.js
+// api/products.js
 import dbConnect from "../../../../util/dbConnect";
 import Product from "../../../../models/Product";
 import Campaign from "../../../../models/Campaign";
 
 const handler = async (req, res) => {
-  await dbConnect();
-  const { method } = req;
+ await dbConnect();
+ const { method } = req;
 
-  switch (method) {
+ switch (method) {
     case "GET":
       try {
-        const productsWithCampaigns = await Product.find({ campaign: { $exists: true, $ne: null } }).populate('campaign');
-        res.status(200).json(productsWithCampaigns);
+
+        // Get all the products with a valid campaign
+        const productWithCampaign = await Product.find().populate('campaign');
+        
+        res.status(200).json(productWithCampaign);       
+
+        res.status(200).json(productWithCampaign);
       } catch (error) {
         console.error(error);
         res.status(500).json({ error: "Internal Server Error" });
@@ -21,7 +26,7 @@ const handler = async (req, res) => {
     default:
       res.status(405).json({ error: "Method Not Allowed" });
       break;
-  }
+ }
 };
 
 export default handler;
