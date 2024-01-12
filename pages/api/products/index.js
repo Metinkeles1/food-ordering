753cbe1d79380f07065a87e4c2652bfb278a5282
Metin Keleş <metinkeles677@gmail.com>
2024-01-12 23:54:16 +0300

    Refactor product API route handler, implement switch for HTTP methods

--- pages/api/products/index.js
+++ pages/api/products/index.js
@@ -6,25 +6,35 @@ const handler = async (req, res) => {
   await dbConnect();
   const { method } = req;
 
-  if (method === "GET") {
-    try {
-      const allProducts = await Product.find();
-      res.status(200).json(allProducts);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
-  }
+  switch (method) {
+    case "GET":
+      try {
+        const allProducts = await Product.find();
+        res.status(200).json(allProducts);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+      break;
 
+    case "POST":
+      try {
+        // Kontrol et: Eğer kampanya belirtilmemişse, varsayılan kampanya "No Campaign" olarak ayarla
+        if (!req.body.campaign) {
+          req.body.campaign = { title: "No Campaign", discount: 0 };
+        }
 
-  if (method === "POST") {
-    try {
-      const newProduct = await Product.create(req.body);
-      res.status(200).json(newProduct);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: "Internal Server Error" });
-    }
+        const newProduct = await Product.create(req.body);
+        res.status(200).json(newProduct);
+      } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: "Internal Server Error" });
+      }
+      break;
+
+    default:
+      res.status(405).json({ error: "Method Not Allowed" });
+      break;
   }
 };
 
