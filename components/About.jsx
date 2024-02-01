2c02c20d238be4f00bae586e2c45c4e4666cfafa
Metin Keleş <metinkeles677@gmail.com>
2024-02-01 19:11:31 +0300

    Refactor About component for dynamic data fetching

--- components/About.jsx
+++ components/About.jsx
@@ -1,30 +1,39 @@
 import Image from "next/image";
 import Title from "./ui/Title";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const About = () => {
-    return (
-        <div className="bg-secondary py-14">
-            <div className="container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse">
-                <div className="flex justify-center">
-                    <div className="relative sm:w-[445px] sm:h-[600px]  flex justify-center w-[370px] h-[610px]">
-                        <Image src="/images/about-img.png" alt="" layout="fill" />
-                    </div>
-                </div>
-                <div className="md:w-1/2 ">
-                    <Title addClass="text-[40px]">We Are Feane</Title>
-                    <p className="my-5 flex flex-col items-center">
-                        There are many variations of passages of Lorem Ipsum available, but
-                        the majority have suffered alteration in some form, by injected
-                        humour, or randomised words which don`t look even slightly
-                        believable. If you are going to use a passage of Lorem Ipsum, you
-                        need to be sure there isn`t anything embarrassing hidden in the
-                        middle of text. All
-                    </p>
-                    <button className="btn-primary">Read More</button>
-                </div>
-            </div>
-        </div>
+  const [aboutData, setaboutData] = useState([]);
+  const getAbout = async () => {
+    const aboutRes = await axios.get(
+      `${process.env.NEXT_PUBLIC_API_URL}/about`
     );
+    setaboutData(aboutRes.data[0]);
+  };
+
+  useEffect(() => {
+    getAbout();
+  }, []);
+
+  return (
+    <div className='bg-secondary py-14'>
+      <div className='container mx-auto flex items-center text-white gap-20 justify-center flex-wrap-reverse'>
+        <div className='flex justify-center'>
+          <div className='relative sm:w-[445px] sm:h-[600px]  flex justify-center w-[370px] h-[610px]'>
+            <Image src={aboutData.img} alt='' layout='fill' />
+          </div>
+        </div>
+        <div className='md:w-1/2 '>
+          <Title addClass='text-[40px]'>{aboutData.title}</Title>
+          <p className='my-5 flex flex-col items-center'>
+            {aboutData.description}
+          </p>
+          <button className='btn-primary'>Read More</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
