de3a59ca48c7458efe9c68ac7441905218112489
Metin Keleş <metinkeles677@gmail.com>
2024-05-30 00:26:01 +0300

    feat(Carousel): :art: add Carousel component with background images and settings
    - Added Carousel component with settings for autoplay, infinite loop, and fade effects - Integrated Slider from react-slick - Included background images for the slider

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -5,72 +5,149 @@ import Link from "next/link";
 
 const Carousel = () => {
   const settings = {
-    dots: true,
+    dots: false,
     infinite: true,
-    speed: 500,
+    speed: 1500,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
-    autoplaySpeed: 35000,
-    appendDots: (dots) => (
-      <div>
-        <ul className='container mx-auto w-full text-start'>{dots}</ul>
-      </div>
-    ),
-    customPaging: (i, currentSlide) => (
-      <div
-        className={`w-3 h-3 rounded-full ${
-          i === currentSlide ? "bg-primary" : "bg-gray-300"
-        }`}
-      ></div>
-    ),
+    autoplaySpeed: 6000,
+    fade: true,
   };
   return (
-    <div className='h-screen w-full -mt-[88px]'>
-      <div className='absolute top-0 left-0 w-full h-full'>
-        <div className='relative h-full w-full'>
+    <div className='lg:h-screen h-[130vh] md:h-[110vh] w-full -mt-[88px]'>
+      <Slider {...settings} className='absolute top-0 left-0 w-full h-full'>
+        <div className='relative w-full h-screen '>
           <Image
             src='/images/hero-bg-2.jpg'
-            alt=''
+            alt='Background Image 2'
             layout='fill'
             objectFit='cover'
             priority
+            className='z-0'
           />
         </div>
-      </div>
-      <div className='absolute top-0 left-0 w-full h-full bg-black opacity-60'></div>
-      <Slider {...settings}>
-        <div>
-          <div className='container mx-auto sm:mt-52 sm:top-48 mt-20 top-20 text-white flex flex-col items-start gap-y-10'>
-            <Title addClass='text-6xl'>Fast Food Restaurant</Title>
-            <p className='text-sm sm:w-full md:w-3/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 '>
-              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
-              sapiente ad mollitia laborum quam quisquam esse error unde.
-              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
-              quos nihil ducimus libero ipsamm.
-            </p>
-
-            <Link href='/menu' className='md:inline-block hidden sm'>
-              <button className='btn-primary'>Order Now</button>
-            </Link>
-          </div>
+        <div className='relative w-full h-screen '>
+          <Image
+            src='/images/hero-bg-img-pizza.jpg'
+            alt='Background Image 2'
+            layout='fill'
+            objectFit='cover'
+            priority
+            className='z-0'
+          />
         </div>
-        <div>
-          <div className='container mx-auto sm:mt-52 sm:top-48 mt-20 top-20 text-white flex flex-col items-start gap-y-10'>
-            <Title addClass='text-6xl'>Fast Food Restaurant</Title>
-            <p className='text-sm sm:w-full md:w-3/2 lg:w-1/2 xl:w-1/2 2xl:w-1/2 '>
-              Doloremque, itaque aperiam facilis rerum, commodi, temporibus
-              sapiente ad mollitia laborum quam quisquam esse error unde.
-              Tempora ex doloremque, labore, sunt repellat dolore, iste magni
-              quos nihil ducimus libero ipsam.
-            </p>
-            <Link href='/menu' className='md:inline-block hidden sm'>
-              <button className='btn-primary'>Order Now</button>
-            </Link>
-          </div>
+        <div className='relative w-full h-screen '>
+          <Image
+            src='/images/hero-bg-img-drinks.jpg'
+            alt='Background Image 2'
+            layout='fill'
+            objectFit='cover'
+            priority
+            className='z-0'
+          />
         </div>
       </Slider>
+      <div className='absolute top-0 left-0 w-full h-full bg-black opacity-60'></div>
+      <div className='absolute w-full h-full text-white'>
+        <div className='container mx-auto w-full'>
+          <div className='flex h-full w-full pt-16'>
+            <div>
+              <Title addClass='text-4xl bg-primary inline-block py-2 px-4 my-4'>
+                Snack Haven
+              </Title>
+              <Title addClass='lg:text-[90px] sm:text-[60px] text-6xl leading-snug mb-4'>
+                Healthy and Tasty Meals, For Healthy living
+              </Title>
+              <p className='text-md my-4 leading-6 text-center'>
+                Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+                sapiente ad mollitia laborum quam quisquam esse error unde.
+                Tempora ex doloremque, labore, sunt repellat dolore, iste magni
+                quos nihil ducimus libero ipsamm.
+              </p>
+              <Link href='/menu' className='md:inline-block hidden w-full'>
+                <button className='btn-primary mt-6 flex items-center justify-center mx-auto'>
+                  Order Now
+                </button>
+              </Link>
+            </div>
+          </div>
+          <div className='w-full lg:mt-24 mt-8 rounded-lg'>
+            <div className='grid grid-cols-12 gap-4 p-4  rounded-3xl'>
+              <div className='lg:col-span-3 col-span-6 p-4 bg-primary rounded-3xl'>
+                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
+                  <Image
+                    src='/images/hero-img-burger-real.png'
+                    alt=''
+                    layout='fill'
+                    objectFit='contain'
+                    priority
+                    className='hover:size-40'
+                  />
+                </div>
+                <Title addClass='text-3xl leading-snug mb-4 mt-2 text-center'>
+                  Hamburger
+                </Title>
+                <p className='text-sm my-4 leading-snug opacity-75'>
+                  Doloremque, itaque aperiam facilis rerum,
+                </p>
+              </div>
+              <div className='lg:col-span-3 col-span-6 p-4 bg-primary rounded-3xl'>
+                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
+                  <Image
+                    src='/images/hero-img-pizza-real.png'
+                    alt=''
+                    layout='fill'
+                    objectFit='contain'
+                    priority
+                  />
+                </div>
+                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                  Pizza
+                </Title>
+                <p className='text-sm my-4 leading-snug opacity-75'>
+                  Doloremque, itaque aperiam facilis rerum,
+                </p>
+              </div>
+              <div className='lg:col-span-3 col-span-6 p-4 bg-primary  rounded-3xl'>
+                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
+                  <Image
+                    src='/images/hero-img-drink-reall.png'
+                    alt=''
+                    layout='fill'
+                    objectFit='contain'
+                    priority
+                  />
+                </div>
+                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                  Drinks
+                </Title>
+                <p className='text-sm my-4 leading-snug opacity-75'>
+                  Doloremque, itaque aperiam facilis rerum,
+                </p>
+              </div>
+              <div className='lg:col-span-3 col-span-6 p-4 bg-primary  rounded-3xl'>
+                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
+                  <Image
+                    src='/images/hero-img-soup-real.png'
+                    alt=''
+                    layout='fill'
+                    objectFit='contain'
+                    priority
+                  />
+                </div>
+                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                  Soup
+                </Title>
+                <p className='text-sm my-4 leading-snug opacity-75'>
+                  Doloremque, itaque aperiam facilis rerum,
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
