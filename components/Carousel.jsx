efe28b221283ad6bcd8250a232e81875927f4467
Metin Keleş <metinkeles677@gmail.com>
2024-05-30 16:33:42 +0300

    style(Carousel): :art: Adjust layout, improve responsiveness, and enhance styles for Carousel and Campaigns components
    - Adjusted the overall height and top margin of the Carousel component for better visual alignment. - Updated text and button positioning, as well as padding, in the Carousel to improve readability and user experience. - Enhanced responsive layout for smaller screens to ensure a consistent look and feel across all devices. - Improved image hover effects and layout consistency in the Carousel component. - Adjusted campaign item container styles and padding in the Campaigns component to improve visual appeal. - Updated text size and layout in Campaigns for better readability. - Filtered and displayed products with valid campaigns in the Campaigns component. - Improved responsive design and grid layout in the Campaigns component to ensure a seamless user experience on all devices."

--- components/Carousel.jsx
+++ components/Carousel.jsx
@@ -16,7 +16,7 @@ const Carousel = () => {
     fade: true,
   };
   return (
-    <div className='lg:h-screen h-[130vh] md:h-[110vh] w-full -mt-[88px]'>
+    <div className='lg:h-[105vh] h-[115vh] md:h-[95vh] w-full -mt-[88px]'>
       <Slider {...settings} className='absolute top-0 left-0 w-full h-full'>
         <div className='relative w-full h-screen '>
           <Image
@@ -52,19 +52,17 @@ const Carousel = () => {
       <div className='absolute top-0 left-0 w-full h-full bg-black opacity-60'></div>
       <div className='absolute w-full h-full text-white'>
         <div className='container mx-auto w-full'>
-          <div className='flex h-full w-full pt-16'>
+          <div className='flex h-full w-full lg:pt-16 pt-2'>
             <div>
               <Title addClass='text-4xl bg-primary inline-block py-2 px-4 my-4'>
                 Snack Haven
               </Title>
-              <Title addClass='lg:text-[90px] sm:text-[60px] text-6xl leading-snug mb-4'>
+              <Title addClass='lg:text-[90px] text-[60px] text-6xl leading-snug mb-4'>
                 Healthy and Tasty Meals, For Healthy living
               </Title>
               <p className='text-md my-4 leading-6 text-center'>
                 Doloremque, itaque aperiam facilis rerum, commodi, temporibus
                 sapiente ad mollitia laborum quam quisquam esse error unde.
-                Tempora ex doloremque, labore, sunt repellat dolore, iste magni
-                quos nihil ducimus libero ipsamm.
               </p>
               <Link href='/menu' className='md:inline-block hidden w-full'>
                 <button className='btn-primary mt-6 flex items-center justify-center mx-auto'>
@@ -73,10 +71,10 @@ const Carousel = () => {
               </Link>
             </div>
           </div>
-          <div className='w-full lg:mt-24 mt-8 rounded-lg'>
-            <div className='grid grid-cols-12 gap-4 p-4  rounded-3xl'>
-              <div className='lg:col-span-3 col-span-6 p-4 bg-primary rounded-3xl'>
-                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
+          <div className='w-full mt-8 rounded-lg'>
+            <div className='grid grid-cols-12 lg:p-4 gap-2 rounded-3xl'>
+              <div className='lg:col-span-3 col-span-6 p-2 bg-primary rounded-3xl'>
+                <div className='relative lg:h-28 h-16 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
                   <Image
                     src='/images/hero-img-burger-real.png'
                     alt=''
@@ -86,15 +84,15 @@ const Carousel = () => {
                     className='hover:size-40'
                   />
                 </div>
-                <Title addClass='text-3xl leading-snug mb-4 mt-2 text-center'>
+                <Title addClass='lg:text-2xl text-lg leading-snug mb-2 mt-2 text-center'>
                   Hamburger
                 </Title>
-                <p className='text-sm my-4 leading-snug opacity-75'>
-                  Doloremque, itaque aperiam facilis rerum,
+                <p className='text-sm leading-snug opacity-75 text-center'>
+                  Doloremque, itaque aperiam
                 </p>
               </div>
-              <div className='lg:col-span-3 col-span-6 p-4 bg-primary rounded-3xl'>
-                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
+              <div className='lg:col-span-3 col-span-6 p-2 bg-primary rounded-3xl'>
+                <div className='relative lg:h-28 h-16 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
                   <Image
                     src='/images/hero-img-pizza-real.png'
                     alt=''
@@ -103,15 +101,15 @@ const Carousel = () => {
                     priority
                   />
                 </div>
-                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                <Title addClass='lg:text-2xl text-lg leading-snug mb-2 mt-2  text-center'>
                   Pizza
                 </Title>
-                <p className='text-sm my-4 leading-snug opacity-75'>
-                  Doloremque, itaque aperiam facilis rerum,
+                <p className='text-sm leading-snug opacity-75 text-center'>
+                  Doloremque, itaque aperiam
                 </p>
               </div>
-              <div className='lg:col-span-3 col-span-6 p-4 bg-primary  rounded-3xl'>
-                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
+              <div className='lg:col-span-3 col-span-6 p-2 bg-primary  rounded-3xl'>
+                <div className='relative lg:h-28 h-16 w-full transition-transform duration-300 ease-in-out transform hover:scale-110 '>
                   <Image
                     src='/images/hero-img-drink-reall.png'
                     alt=''
@@ -120,15 +118,15 @@ const Carousel = () => {
                     priority
                   />
                 </div>
-                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                <Title addClass='lg:text-2xl text-lg leading-snug mb-2 mt-2  text-center'>
                   Drinks
                 </Title>
-                <p className='text-sm my-4 leading-snug opacity-75'>
-                  Doloremque, itaque aperiam facilis rerum,
+                <p className='text-sm leading-snug opacity-75 text-center'>
+                  Doloremque, itaque aperiam
                 </p>
               </div>
-              <div className='lg:col-span-3 col-span-6 p-4 bg-primary  rounded-3xl'>
-                <div className='relative h-28 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
+              <div className='lg:col-span-3 col-span-6 p-2 bg-primary  rounded-3xl'>
+                <div className='relative lg:h-28 h-16 w-full transition-transform duration-300 ease-in-out transform hover:scale-110'>
                   <Image
                     src='/images/hero-img-soup-real.png'
                     alt=''
@@ -137,11 +135,11 @@ const Carousel = () => {
                     priority
                   />
                 </div>
-                <Title addClass='text-3xl leading-snug mb-4 mt-2  text-center'>
+                <Title addClass='lg:text-2xl text-lg leading-snug mb-2 mt-2  text-center'>
                   Soup
                 </Title>
-                <p className='text-sm my-4 leading-snug opacity-75'>
-                  Doloremque, itaque aperiam facilis rerum,
+                <p className='text-sm leading-snug opacity-75 text-center'>
+                  Doloremque, itaque aperiam
                 </p>
               </div>
             </div>
